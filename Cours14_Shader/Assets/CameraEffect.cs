1ad3480b2b0db51386627d821128fa0895cac410
JORDAN\Utilisateur <jordan.cote@outlook.com>
2020-10-26 12:48:07 -0400

    Termination

---
+++ Cours14_Shader/Assets/CameraEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour
+{
+
+    private Material leMat;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        leMat = new Material(Shader.Find("Hidden/Image_EFFECT_SHADER"));
+    }
+
+    // Update is called once per frame
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, leMat);
+    }
+}
